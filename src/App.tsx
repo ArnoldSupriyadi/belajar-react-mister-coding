1fe7516287e69cc7a7b22915bdf032465f16529a
ArnoldSupriyadi <arnoldsupriyadi@gmail.com>
2026-04-01 23:55:43 +0700

    Use Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import './App.css'
 import LatihanForm from './components/features/form/LatihanForm';
 import LatihanForm2 from './components/features/form/LatihanForm2';
+import Input from './components/input/Input';
+import { useState, useEffect } from 'react';
 
 
 
 function App()  {
+  const [nama, setNama] = useState<string>("");
+
+  useEffect(() => {
+    console.log('name :', nama)
+  }, [nama])
+
   return (
+  
     <div className='App'>
+    <Input value={nama} placeholder='Masukan Nama Kamu' onChange={(e)=>
+    {
+        setNama(e.target.value)
+    }} />
+    <p>{nama}</p>
+    <button onClick={() => setNama("Jamet")}>Test UseEffect</button>
         {/* <ContohList /> */}
         {/* <ContohList2 /> */}
-        <LatihanForm2 />
+        {/* <LatihanForm2 /> */}
     </div>
   );
 }
